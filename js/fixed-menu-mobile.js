c52b3f6f1b2debed5fd9b4d4176c8efb1252e585
Tais de Souza Lessa <tais@mozillafoundation.org>
2017-05-18 14:22:00 -0700

    changes on complex model

--- js/fixed-menu-mobile.js
+++ js/fixed-menu-mobile.js
@@ -1,8 +1,10 @@
 
 $(window).on('scroll', function () {
-    if ($(window).scrollTop() >= 324) {
+    if ($(window).scrollTop() >= 412) {
         $('.dropdown').addClass('fixed');
+        $('.margin-top-mob').addClass('fixed');
     } else {
         $('.dropdown').removeClass('fixed');
+        $('.margin-top-mob').removeClass('fixed');
     }
 });
